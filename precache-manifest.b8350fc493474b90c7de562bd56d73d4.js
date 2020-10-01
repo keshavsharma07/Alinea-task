79906feef6c05d04c4a63f81ad453c34cace1272
Keshav Sharma <keshavgalav07@gmail.com>
2020-10-01 23:39:54 +0530

    Updates

---
+++ precache-manifest.b8350fc493474b90c7de562bd56d73d4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3209969d06da8132e9e87aef38cabc4",
+    "url": "/Alinea-task/index.html"
+  },
+  {
+    "revision": "fb6a964fcc81ced017ce",
+    "url": "/Alinea-task/static/css/2.69aac891.chunk.css"
+  },
+  {
+    "revision": "0bc7eedf403f3098adf5",
+    "url": "/Alinea-task/static/css/main.32acc2b8.chunk.css"
+  },
+  {
+    "revision": "fb6a964fcc81ced017ce",
+    "url": "/Alinea-task/static/js/2.d91ad43b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Alinea-task/static/js/2.d91ad43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc7eedf403f3098adf5",
+    "url": "/Alinea-task/static/js/main.17db7d52.chunk.js"
+  },
+  {
+    "revision": "7cf527b4266ed8d91c38",
+    "url": "/Alinea-task/static/js/runtime-main.a8021797.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Alinea-task/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Alinea-task/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Alinea-task/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Alinea-task/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Alinea-task/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
